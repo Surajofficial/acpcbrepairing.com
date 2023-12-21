4fb139be51c9bc8a25d60b91a7c61b9c4588904b
surajofficial <surajofficial694@gmail.com>
2023-12-21 12:29:31 +0530

    added content on home page

--- index.php
+++ index.php
@@ -42,16 +42,7 @@ include('common/slider.inc.php');
     <div class="col-lg-4 about-img wow fadeInUp">
       <?php include("common/enquiry.inc.php"); ?>
     </div>
-    <div class="col-lg-12 content wow fadeInUp pt-md-4">
-      <h1 align="left">AC Repairing course</h1>
-      <p style="text-align:justify;">
-        In AC PCB Repairng Course we will be discussing about the AC Repairing course. The objective of this course is to provide the students with the necessary skills and knowledge required for repairing air conditioners. The course covers topics such as AC servicing, troubleshooting and repair, refrigeration cycle and more. It is a hands-on course that will enable the students to put their knowledge into practice. So if you are looking for a career in AC repairing, then this course is definitely for you. Read on to find out more about what the course has to offer.
-      </p>
-      <h2>What is AC Repairing?</h2>
-      <p style="text-align:justify;">AC repairing is a process of maintaining and repairing cooling systems such as refrigerators, freezers, and air conditioners. The aim of this process is to keep these machines running smoothly and efficiently. There are different aspects to AC repair, which include troubleshooting, cleaning, and replacement parts. In most cases, the first step in AC repair is to identify the problem. Once the problem has been identified, the next step is to find a solution. In some cases, the problem may be something as simple as a loose connection or a clogged filter. In other cases, however, the problem may be more serious and require more extensive repairs.
-      </p>
 
-    </div>
   </div>
 </div>
 <section class="bgmk">
@@ -78,8 +69,54 @@ include('common/slider.inc.php');
 </section>
 
 
-</div>
 <?php include("common/award.inc.php"); ?>
+
 <?php include("common/testimonial.inc.php"); ?>
+<div class="container-fluid py-md-4 pt-md-5">
+  <div class="row">
+    <div class="col-lg-6 content wow fadeInUp pt-md-4">
+      <h3 align="left">AC Repairing Institute in Delhi</h3>
+      <p style="text-align:justify;">
+        Explore the leading AC Repairing Institute in Delhi, committed to providing an all-encompassing curriculum and practical training in air conditioning technology. Our institute serves as a guiding light for aspiring technicians, offering expert mentorship and hands-on skills crucial for mastering AC repair techniques. Join our esteemed institute to delve into AC system intricacies, laying a robust foundation for a successful career in HVAC maintenance and repair.
+      </p>
+      <h3>AC Repair Course in Delhi</h3>
+      <p style="text-align:justify;">Enroll in our meticulously designed AC Repair Course in Delhi covering all facets of AC systems. Gain an in-depth understanding of repair methods, troubleshooting, and maintenance through a blend of practical and theoretical sessions. Our curriculum ensures proficiency in handling diverse AC models and effectively resolving complex issues.
+      </p>
+    </div>
+    <div class="col-lg-6 content wow fadeInUp pt-md-4">
+      <h3>Why Choose Us for AC Repairing Training?
+      </h3>
+      <ul>
+        <li><strong>Comprehensive Curriculum:</strong> Stay updated with industry trends and technological advancements through our extensive syllabus.</li>
+        <li><strong>Expert Faculty:</strong> Learn from seasoned professionals with vast experience in AC repair, offering personalized guidance.</li>
+        <li><strong>Hands-on Training:</strong> Gain practical expertise in our state-of-the-art labs, simulating real-world repair scenarios.</li>
+        <li><strong>Placement Assistance:</strong> Benefit from our strong industry connections, facilitating job placements and internships.</li>
+        <li><strong>Flexible Learning:</strong> Choose from flexible timings and course schedules tailored to your convenience.</li>
+        <li><strong>Certification:</strong> Receive recognized certifications upon completion, validating your expertise.</li>
+      </ul>
+
+    </div>
+    <div class="col-lg-12 content wow fadeInUp pt-md-4 text-center">
+      <h3>Benefits of Our AC Repairing Course
+      </h3>
+    </div>
+    <div class="col-lg-6 content wow fadeInUp pt-md-4">
+      <ul>
+        <li><b>Skill Enhancement:</b> Master the art of understanding, fixing, and maintaining various air conditioning systems.</li>
+        <li><b>Industry Relevance:</b> Stay updated with the latest technologies and industry trends, boosting employability.</li>
+        <li><b>Career Opportunities:</b> Unlock diverse job prospects in HVAC companies or freelance technician roles.</li>
+      </ul>
+    </div>
+    <div class="col-lg-6 content wow fadeInUp pt-md-4">
+      <ul>
+        <ul>
+          <li><b>Practical Experience:</b> Learn by doing real tasks in our practical training, enhancing real-world problem-solving skills.</li>
+          <li><b>Critical Thinking:</b> Develop problem-solving abilities for diagnosing and resolving complex AC issues.</li>
+          <li><b>Certification:</b> Acquire recognized certifications validating your expertise in AC repair.</li>
+        </ul>
+    </div>
+
+  </div>
+</div>
 <?php include("common/subscribe.inc.php"); ?>
 <?php include('common/footer.inc.php'); ?>
